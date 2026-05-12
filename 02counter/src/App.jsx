eef8825eed5ab40f0e5597de2468ae762feee49d
playeron1 <krishnshah2003@gmail.com>
2026-05-12 17:01:06 +0530

    created counter app for practice

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Krish & React</h2>
+      <h3>Counter Value : 5</h3> 
+      <button>Add value</button>
+      <button>remove value</button>     
+    </>
+  )
+}
+
+export default App
